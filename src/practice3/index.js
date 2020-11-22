6c4bbe2f3c053630fc6d76e2492b847cc63fa836
ZW <zhaowuwei2020@foxmail.com>
2020-11-22 16:35:43 +0800

    [ZWW]: Finish practice 3.

--- src/practice3/index.js
+++ src/practice3/index.js
@@ -1,1 +1,26 @@
-export const getPoetry
+// // Promise
+// // eslint-disable-next-line import/prefer-default-export
+// export const getPoetry = () => {
+//   return fetch("https://v1.jinrishici.com/all.json")
+//     .then((response) => {
+//       if (response.status !== 200) {
+//         throw new Error();
+//       }
+//       return Promise.resolve(response.json());
+//     })
+//     .then((data) => {
+//       return [data.origin, data.author, data.content];
+//     });
+// };
+
+// Async
+// eslint-disable-next-line import/prefer-default-export
+export const getPoetry = async () => {
+  try {
+    const response = await fetch("https://v1.jinrishici.com/all.json");
+    const data = await response.json();
+    return [data.origin, data.author, data.content];
+  } catch {
+    throw new Error();
+  }
+};
